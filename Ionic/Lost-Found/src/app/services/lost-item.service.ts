527aaf11f23e9e1433bcf6a127b45978a860c03e
Zun Hom <zunhom2050@gmail.com>
2024-07-27 16:10:31 +0800

    t

--- Ionic/Lost-Found/src/app/services/lost-item.service.ts
+++ Ionic/Lost-Found/src/app/services/lost-item.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class LostItemService {
-  private apiUrl = 'http://localhost/processItem.php';
+  private apiUrl = 'http://localhost/site/processItem.php';
 
   constructor(private http: HttpClient) { }
 
