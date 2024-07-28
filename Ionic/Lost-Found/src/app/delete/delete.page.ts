5e68897bd8c65becb7c3272b0af75d9b3b197023
Zun Hom <zunhom2050@gmail.com>
2024-07-29 03:55:03 +0800

    Update delete.page.ts

--- Ionic/Lost-Found/src/app/delete/delete.page.ts
+++ Ionic/Lost-Found/src/app/delete/delete.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import {
   IonContent,
   IonHeader,
@@ -9,7 +10,12 @@ import {
   IonButtons,
   IonBackButton,
   IonButton,
-  IonIcon
+  IonIcon,
+  IonList,
+  IonItem,
+  IonThumbnail,
+  IonLabel,
+  IonFooter,
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -27,13 +33,75 @@ import {
     IonButtons,
     IonBackButton,
     IonButton,
-    IonIcon
-  ]
+    IonIcon,
+    HttpClientModule,
+    IonList,
+    IonItem,
+    IonThumbnail,
+    IonLabel,
+    IonFooter,
+  ],
 })
 export class DeletePage implements OnInit {
+  items: any[] = [];
+  paginatedItems: any[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+  totalPages: number = 0;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.fetchItems();
+  }
 
+  fetchItems() {
+    this.http.get<any[]>('http://localhost/getItem.php').subscribe(
+      (data) => {
+        console.log('Items fetched:', data); // Debugging line
+        this.items = data.map(item => ({
+          ...item,
+          image_path: `http://localhost/${item.image_path}` // Ensure the path is correct
+        }));
+        this.totalPages = Math.ceil(this.items.length / this.itemsPerPage);
+        this.paginateItems();
+      },
+      (error) => {
+        console.error('Error fetching items:', error);
+      }
+    );
+  }
+
+  paginateItems() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    this.paginatedItems = this.items.slice(start, end);
+    console.log('Paginated items:', this.paginatedItems); // Debugging line
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.paginateItems();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.paginateItems();
+    }
+  }
+
+  deleteItem(itemId: string) {
+    this.http.post('http://localhost/processDelete.php', { id: itemId }).subscribe(
+      (response: any) => {
+        console.log('Item deleted:', response); // Debugging line
+        this.fetchItems();
+      },
+      (error) => {
+        console.error('Error deleting item:', error);
+      }
+    );
+  }
 }
